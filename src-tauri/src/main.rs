3f8762454ccf4909ee779d78955ab3e7f324db96
ninju <74989292+ninjubaer@users.noreply.github.com>
2025-01-18 16:01:49 +0100

    initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,35 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+
+#[tauri::command]
+async fn drag_window(window: tauri::Window) {
+    window.start_dragging().unwrap();
+}
+#[tauri::command]
+async fn minimize_window(window: tauri::Window) {
+    window.minimize().unwrap();
+}
+#[tauri::command]
+async fn close_window(window: tauri::Window) {
+    window.close().unwrap();
+}
+
+fn main() {
+  tauri::Builder::default()
+      .setup(|app| {
+          let window = app.get_window("main").unwrap();
+          let _ = window.set_title("VolleyGroups");
+          let _ = window.set_focus();
+          Ok(())
+      })
+      .invoke_handler(tauri::generate_handler![
+          drag_window,
+          minimize_window,
+          close_window
+      ])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
+}
